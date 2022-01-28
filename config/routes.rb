72b60e1b756223f8196830e50841830a69fd692d
Leon Langhoff <leon.langhoff@posteo.de>
2022-01-28 12:52:43 +0100

    added devise with custom turbo setup due to rails 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  # custom devise_turbo setup
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
   # Defines the root path route ("/")
+  root to: 'pages#home'
   # root "articles#index"
 end
