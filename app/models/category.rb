30e862900aa070919247e2b1ae1cd4ea61007516
Leon Langhoff <leon.langhoff@posteo.de>
2022-02-02 15:23:39 +0100

    added basic validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   belongs_to :topic
   has_many :offers, through: :offers_categories
+  validates :title, presence: true
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,5 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many :categories, through: :offers_categories
+  validates :title, :description, :about_us, :email, :postcode, presence: true
 end
