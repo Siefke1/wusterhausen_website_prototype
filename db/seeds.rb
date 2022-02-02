fe86e7bc49172a1351ea4f389af326a874bc655f
eric <draheim.eric@googlemail.com>
2022-02-02 16:16:30 +0100

    Seeds sind Ferdsch

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+# Delete
+#  Offer.delete_all
+#  Category.delete_all
+# Topic.delete_all
+# p "Everything is Deleted"
+
+# New Seeds
+User.create!(email: 'peter@pan.de', password: 'peter@pan.de')
+4.times do |i|
+  Topic.create!(
+    title: Faker::Hobby.activity
+  )
+end
+
+20.times do |i|
+Category.create!(
+  title: Faker::Hobby.activity,
+  topic_id: 1
+)
+end
+
+40.times do |i|
+  Offer.create!(
+    title: Faker::Company.name,
+    description: Faker::Lorem.sentence(word_count: 20),
+    about_us: Faker::Lorem.sentence(word_count: 20),
+    email: Faker::Internet.email,
+    address: Faker::Address.street_address,
+    postcode: Faker::Address.postcode,
+    phone: Faker::PhoneNumber.cell_phone,
+    status: 1,
+    user_id: 1
+)
+end
+
+p "there are new seeds"
