8a77553526e81ead52adcfc615164530d10c51b4
Leon Langhoff <leon.langhoff@posteo.de>
2022-02-07 15:55:58 +0100

    fixed bug in dashboard show

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,6 @@ class DashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = current_user
     if current_user == "institution"
       @offers = current_user.offers
     else
