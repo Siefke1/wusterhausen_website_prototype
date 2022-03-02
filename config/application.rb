1fb993ad8ac35da15184785c3e3eb2454aca72f8
Leon Langhoff <leon.langhoff@posteo.de>
2022-03-02 12:47:42 +0100

    user mailer preview config

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module WusterhausenWebsitePrototype
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.autoloader = :zeitwerk
-
+    config.action_mailer.preview_path = "#{Rails.root}/app/mailers/previews"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
