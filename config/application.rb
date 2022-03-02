fd1097fc563a24e01e79da62998e593bf5e19208
Leon Langhoff <leon.langhoff@posteo.de>
2022-03-02 12:29:55 +0100

    config autoloader

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 module WusterhausenWebsitePrototype
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 7.0
-    config.autoload = :classic
+    config.load_defaults 7.0
+    # config.autoload = :classic
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
