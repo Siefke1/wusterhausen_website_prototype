95e34e4f228472d963269b0ba0a6f5fd786b6495
Leon Langhoff <leon.langhoff@posteo.de>
2022-03-02 12:17:35 +0100

    fix attempt 2

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 module WusterhausenWebsitePrototype
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    # config.load_defaults 7.0
     config.autoload = :classic
     # Configuration for the application, engines, and railties goes here.
     #
