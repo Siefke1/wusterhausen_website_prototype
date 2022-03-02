795ca7e12638156a96cee2e5ef8816a0900a1675
Leon Langhoff <leon.langhoff@posteo.de>
2022-03-02 12:38:57 +0100

    config zeitwerk autoloader

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ module WusterhausenWebsitePrototype
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    # config.autoload = :classic
+    config.autoloader = :zeitwerk
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
