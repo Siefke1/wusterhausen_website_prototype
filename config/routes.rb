f9e9e896367c1aed9e0bae09628f33e1699d3f55
eric <draheim.eric@googlemail.com>
2022-03-02 01:45:35 +0100

    article style

--- config/routes.rb
+++ config/routes.rb
@@ -47,4 +47,11 @@ Rails.application.routes.draw do
 
   resources :articles, except: :destroy
   delete "/articles/:id", to: "articles#destroy", as: :delete_article
+
+  #sidekiq
+
+  require "sidekiq/web"
+  authenticate :user, ->(user) { user.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
